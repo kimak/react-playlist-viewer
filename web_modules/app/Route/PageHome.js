94e938f9578a0d761c1add2b948c7de0cadc7642
DEGUINE <benoit.deguine@sdw-104-08.local>
2016-05-25 16:19:54 +0200

    init

---
+++ web_modules/app/Route/PageHome.js
@@ -0,0 +1,13 @@
+import React, {PropTypes, Component } from 'react';
+
+export default class PageHome extends Component {
+
+  render() {
+    return (
+      <div>
+        Welcome Home
+      </div>
+    )
+  }
+
+}
